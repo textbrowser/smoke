4d46f26c0106454ada01f2c1a91b0e1cbad8a78d
Monster <textbrowser@gmail.com>
2016-12-18 13:54:07 -0500

    New file.

---
+++ Smoke/app/src/main/java/org/purple/smoke/Listeners.java
@@ -0,0 +1,13 @@
+package org.purple.smoke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Listeners extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listeners);
+    }
+}
