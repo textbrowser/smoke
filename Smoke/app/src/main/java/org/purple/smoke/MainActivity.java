b8b43b7f9455d080e0ff8fc06d3b222c1b5ce8b8
Monster <textbrowser@gmail.com>
2016-12-19 18:11:09 -0500

    Clear-text button method.

--- Smoke/app/src/main/java/org/purple/smoke/MainActivity.java
+++ Smoke/app/src/main/java/org/purple/smoke/MainActivity.java
@@ -2,6 +2,9 @@ package org.purple.smoke;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.clear_chat);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final TextView textView = (TextView) findViewById(R.id.chat_messages);
+
+                textView.setText("");
+                }
+            });
     }
 }
