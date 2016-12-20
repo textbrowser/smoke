c6cca2cd2aa1207e316294a7e0b44261d3c3ff3d
Monster <textbrowser@gmail.com>
2016-12-20 15:12:21 -0500

    Chat/Settings functional menus.

--- Smoke/app/src/main/java/org/purple/smoke/Settings.java
+++ Smoke/app/src/main/java/org/purple/smoke/Settings.java
@@ -1,7 +1,10 @@
 package org.purple.smoke;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_chat) {
+            final Intent intent = new Intent(Settings.this, Main.class);
+
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
