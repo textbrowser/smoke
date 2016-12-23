d4ace35b843104c7b8e71dd45f1825f9dff4eb08
Monster <textbrowser@gmail.com>
2016-12-23 17:49:06 -0500

    New Neighbors transport widget. Renamed some widgets.

--- Smoke/app/src/main/java/org/purple/smoke/Settings.java
+++ Smoke/app/src/main/java/org/purple/smoke/Settings.java
@@ -3,14 +3,14 @@ package org.purple.smoke;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
@@ -24,12 +24,14 @@ public class Settings extends AppCompatActivity {
 
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                final RadioButton radioButton1 = (RadioButton) findViewById(R.id.neighbor_ipv4);
-                final TextView textView1 = (TextView) findViewById(R.id.neighbor_ip_address);
-                final TextView textView2 = (TextView) findViewById(R.id.neighbor_port);
-                final TextView textView3 = (TextView) findViewById(R.id.neighbor_scope_id);
+                final RadioButton radioButton1 = (RadioButton) findViewById(R.id.neighbors_ipv4);
+                final Spinner spinner1 = (Spinner) findViewById(R.id.neighbors_transport);
+                final TextView textView1 = (TextView) findViewById(R.id.neighbors_ip_address);
+                final TextView textView2 = (TextView) findViewById(R.id.neighbors_port);
+                final TextView textView3 = (TextView) findViewById(R.id.neighbors_scope_id);
 
                 radioButton1.setChecked(true);
+                spinner1.setSelection(0);
                 textView1.setText("");
                 textView2.setText("4710");
                 textView3.setText("");
@@ -45,20 +47,33 @@ public class Settings extends AppCompatActivity {
         button2 = (Button) findViewById(R.id.refresh_neighbors);
         button2.setEnabled(false);
 
-        RadioButton radioButton1 = (RadioButton) findViewById(R.id.neighbor_ipv4);
+        RadioButton radioButton1 = (RadioButton) findViewById(R.id.neighbors_ipv4);
 
         radioButton1.setEnabled(false);
-        radioButton1 = (RadioButton) findViewById(R.id.neighbor_ipv6);
+        radioButton1 = (RadioButton) findViewById(R.id.neighbors_ipv6);
         radioButton1.setEnabled(false);
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.neighbors_transport);
+
+        spinner1.setEnabled(false);
+
+        String array[] = new String[] {
+                "TCP", "UDP"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this,
+                android.R.layout.simple_spinner_item, array);
+
+        spinner1.setAdapter(adapter);
 
         final RadioGroup radioGroup1 = (RadioGroup) findViewById(R.id.neighbors_ipv_radio_group);
 
         radioGroup1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
         {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                final TextView textView1 = (TextView) findViewById(R.id.neighbor_scope_id);
+                final TextView textView1 = (TextView) findViewById(R.id.neighbors_scope_id);
 
-                if(checkedId == R.id.neighbor_ipv4) {
+                if(checkedId == R.id.neighbors_ipv4) {
                     textView1.setText("");
                     textView1.setVisibility(View.GONE);
                 }
@@ -67,14 +82,14 @@ public class Settings extends AppCompatActivity {
             }
         });
 
-        TextView textView1 = (TextView) findViewById(R.id.neighbor_scope_id);
+        TextView textView1 = (TextView) findViewById(R.id.neighbors_scope_id);
 
         textView1.setEnabled(false);
         textView1.setVisibility(View.GONE);
-        textView1 = (TextView) findViewById(R.id.neighbor_port);
+        textView1 = (TextView) findViewById(R.id.neighbors_port);
         textView1.setEnabled(false);
         textView1.setText("4710");
-        textView1 = (TextView) findViewById(R.id.neighbor_ip_address);
+        textView1 = (TextView) findViewById(R.id.neighbors_ip_address);
         textView1.setEnabled(false);
         textView1.requestFocus();
     }
