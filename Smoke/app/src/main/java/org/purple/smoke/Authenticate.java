c6bc52eaf819ec2847e384d976f9757093f29701
Monster <textbrowser@gmail.com>
2016-12-27 20:06:11 -0500

    Removed import statement.

--- Smoke/app/src/main/java/org/purple/smoke/Authenticate.java
+++ Smoke/app/src/main/java/org/purple/smoke/Authenticate.java
@@ -3,7 +3,6 @@ package org.purple.smoke;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
