56398f556e39ceec74e522bae584a5c44a19a9e5
Monster <textbrowser@gmail.com>
2016-12-31 16:36:13 -0500

    Override.

--- Smoke/app/src/main/java/org/purple/smoke/Database.java
+++ Smoke/app/src/main/java/org/purple/smoke/Database.java
@@ -12,13 +12,16 @@ public class Database extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    @Override
     public void onCreate(SQLiteDatabase db) {
     }
 
+    @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         onCreate(db);
     }
 
+    @Override
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         onUpgrade(db, oldVersion, newVersion);
     }
