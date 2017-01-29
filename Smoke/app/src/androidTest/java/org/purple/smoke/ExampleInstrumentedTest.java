5f78f4d8415b6ce92ffd63ac5327185bd9a4f1c0
Monster <textbrowser@gmail.com>
2017-01-29 09:22:37 -0500

    Test imports.

--- Smoke/app/src/androidTest/java/org/purple/smoke/ExampleInstrumentedTest.java
+++ Smoke/app/src/androidTest/java/org/purple/smoke/ExampleInstrumentedTest.java
@@ -1,6 +1,8 @@
 package org.purple.smoke;
 
 import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
