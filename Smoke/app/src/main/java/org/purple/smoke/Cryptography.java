0d942af4ff1287d77950306806c81b6320144d01
Monster <textbrowser@gmail.com>
2017-02-01 17:29:18 -0500

    Static method.

--- Smoke/app/src/main/java/org/purple/smoke/Cryptography.java
+++ Smoke/app/src/main/java/org/purple/smoke/Cryptography.java
@@ -142,38 +142,6 @@ public class Cryptography
 	return stringBuffer.toString();
     }
 
-    public String publicKeyFingerPrint(PublicKey publicKey)
-    {
-	String fingerprint =
-	    "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc" +
-	    "83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd4" +
-	    "7417a81a538327af927da3e";
-	StringBuffer stringBuffer = new StringBuffer();
-
-	if(publicKey != null)
-	{
-	    byte bytes[] = sha512(publicKey.getEncoded());
-
-	    if(bytes != null)
-		fingerprint = Miscellaneous.byteArrayAsHexString(bytes);
-	}
-
-	try
-	{
-	    for(int i = 0; i < fingerprint.length(); i += 2)
-		if(i < fingerprint.length() - 2)
-		    stringBuffer.append(fingerprint.substring(i, i + 2)).
-			append(":");
-		else
-		    stringBuffer.append(fingerprint.substring(i, i + 2));
-	}
-	catch(Exception exception)
-	{
-	}
-
-	return stringBuffer.toString();
-    }
-
     public byte[] etm(byte data[])
     {
 	/*
@@ -354,6 +322,38 @@ public class Cryptography
 	    ("PBKDF2WithHmacSHA1");
 
 	return secretKeyFactory.generateSecret(keySpec);
+    }
+
+    public static String publicKeyFingerPrint(PublicKey publicKey)
+    {
+	String fingerprint =
+	    "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc" +
+	    "83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd4" +
+	    "7417a81a538327af927da3e";
+	StringBuffer stringBuffer = new StringBuffer();
+
+	if(publicKey != null)
+	{
+	    byte bytes[] = sha512(publicKey.getEncoded());
+
+	    if(bytes != null)
+		fingerprint = Miscellaneous.byteArrayAsHexString(bytes);
+	}
+
+	try
+	{
+	    for(int i = 0; i < fingerprint.length(); i += 2)
+		if(i < fingerprint.length() - 2)
+		    stringBuffer.append(fingerprint.substring(i, i + 2)).
+			append(":");
+		else
+		    stringBuffer.append(fingerprint.substring(i, i + 2));
+	}
+	catch(Exception exception)
+	{
+	}
+
+	return stringBuffer.toString();
     }
 
     public static String randomBytesAsBase64(int length)
