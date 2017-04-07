aa9adf97fda28b78c261d85d4f05eff536f42327
Monster <textbrowser@gmail.com>
2017-04-07 13:28:15 -0400

    Support multiple devices.

---
+++ adb.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+adb="${HOME}/Android/Sdk/platform-tools/adb"
+
+$adb devices | sort -u | grep 'device$' | while read line
+do
+    device=$(echo $line | awk '{print $1}')
+    $adb -s $device $@ &
+    wait
+done
