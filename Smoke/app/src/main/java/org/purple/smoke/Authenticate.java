e721fee7407bf5b8666085dc82e3ce294be5280f
Monster <textbrowser@gmail.com>
2017-04-23 18:59:06 -0400

    Set neighbors_echo in State object.

--- Smoke/app/src/main/java/org/purple/smoke/Authenticate.java
+++ Smoke/app/src/main/java/org/purple/smoke/Authenticate.java
@@ -330,6 +330,9 @@ public class Authenticate extends AppCompatActivity
 	super.onCreate(savedInstanceState);
 	m_databaseHelper = Database.getInstance(getApplicationContext());
         setContentView(R.layout.activity_authenticate);
+	State.getInstance().setNeighborsEcho
+	    (m_databaseHelper.readSetting(null, "neighbors_echo").
+	     equals("true"));
 	prepareListeners();
 
 	boolean isAuthenticated = State.getInstance().isAuthenticated();
