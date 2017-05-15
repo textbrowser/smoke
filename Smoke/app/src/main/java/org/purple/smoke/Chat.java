658dc153c82933bfcb8a00d1a4409f432cb94be5
Monster <textbrowser@gmail.com>
2017-05-15 17:59:24 -0400

    Removed some comments.

--- Smoke/app/src/main/java/org/purple/smoke/Chat.java
+++ Smoke/app/src/main/java/org/purple/smoke/Chat.java
@@ -631,8 +631,6 @@ public class Chat extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
-
 	getMenuInflater().inflate(R.menu.chat_menu, menu);
         return true;
     }
@@ -672,10 +670,6 @@ public class Chat extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
 	int id = item.getItemId();
 
         if(id == R.id.action_settings)
