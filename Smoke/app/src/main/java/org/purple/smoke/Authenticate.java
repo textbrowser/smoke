c62d902d25e812323f651ec554195204c4069ca0
Monster <textbrowser@gmail.com>
2017-05-20 13:18:21 -0400

    Not necessary.

--- Smoke/app/src/main/java/org/purple/smoke/Authenticate.java
+++ Smoke/app/src/main/java/org/purple/smoke/Authenticate.java
@@ -131,7 +131,6 @@ public class Authenticate extends AppCompatActivity
 		    Miscellaneous.showErrorDialog
 			(Authenticate.this,
 			 "Incorrect password. Please try again.");
-		    textView1.setSelectAllOnFocus(true);
 		    textView1.requestFocus();
 		    return;
 		}
