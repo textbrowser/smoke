58d34598ef7c34af06e6faf9618eb9d323167532
Monster <textbrowser@gmail.com>
2017-07-07 21:11:58 -0400

    New release name.

--- Smoke/app/src/main/java/org/purple/smoke/About.java
+++ Smoke/app/src/main/java/org/purple/smoke/About.java
@@ -32,7 +32,7 @@ import android.os.Build;
 public class About
 {
     private final static String s_about =
-	"Version 2017.07.07 Ink " +
+	"Version 2017.07.07 Locke " +
 	(BuildConfig.DEBUG ? "(Debug) " : "(Release)") +
 	"\nAndroid " + Build.VERSION.RELEASE;
 
