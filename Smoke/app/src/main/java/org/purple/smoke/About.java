81dc09741c04ce59e3dce453c04343140e3ba6d0
Monster <textbrowser@gmail.com>
2018-01-04 21:33:46 -0500

    Version.

--- Smoke/app/src/main/java/org/purple/smoke/About.java
+++ Smoke/app/src/main/java/org/purple/smoke/About.java
@@ -32,7 +32,7 @@ import android.os.Build;
 public class About
 {
     private final static String s_about =
-	"Version 2018.01.01 Furious Fire " +
+	"Version 2018.01.05 Furious Fire " +
 	(BuildConfig.DEBUG ? "(Debug) " : "(Release)") +
 	"\nAndroid " + Build.VERSION.RELEASE;
 
