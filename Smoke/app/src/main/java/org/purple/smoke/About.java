a7109a6b2cd690eea216a2b45d431903f2175a81
Monster <textbrowser@gmail.com>
2018-02-01 13:57:36 -0500

    New date.

--- Smoke/app/src/main/java/org/purple/smoke/About.java
+++ Smoke/app/src/main/java/org/purple/smoke/About.java
@@ -32,7 +32,7 @@ import android.os.Build;
 public class About
 {
     private final static String s_about =
-	"Version 2018.01.30 Drooling Dragon " +
+	"Version 2018.02.10 Drooling Dragon " +
 	(BuildConfig.DEBUG ? "(Debug) " : "(Release)") +
 	"\nAndroid " + Build.VERSION.RELEASE;
 
