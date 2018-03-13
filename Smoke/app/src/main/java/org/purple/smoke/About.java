3a2e13d392b006ebf0f5a10d163779b2dd1d8b09
Monster <textbrowser@gmail.com>
2018-03-13 18:52:22 -0400

    New version.

--- Smoke/app/src/main/java/org/purple/smoke/About.java
+++ Smoke/app/src/main/java/org/purple/smoke/About.java
@@ -32,7 +32,7 @@ import android.os.Build;
 public class About
 {
     private final static String s_about =
-	"Version 2018.03.03 Ruby Rosemary " +
+	"Version 2018.03.15 Jane Jetway " +
 	(BuildConfig.DEBUG ? "(Debug) " : "(Release)") +
 	"\nAndroid " + Build.VERSION.RELEASE +
 	(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP ?
