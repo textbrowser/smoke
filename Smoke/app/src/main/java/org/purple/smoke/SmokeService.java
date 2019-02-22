ddbaddaed2893de055a0509861b16baeff054581
textbrowser@gmail.com <textbrowser@gmail.com>
2019-02-22 16:57:20 -0500

    Removed.

--- Smoke/app/src/main/java/org/purple/smoke/SmokeService.java
+++ Smoke/app/src/main/java/org/purple/smoke/SmokeService.java
@@ -33,7 +33,6 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
-import android.util.Log;
 
 public class SmokeService extends Service
 {
