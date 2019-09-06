3983e9e529d6a9e03c8e6760cd020869a588b25f
textbrowser@gmail.com <textbrowser@gmail.com>
2019-09-06 16:03:55 -0400

    Normal text.

--- Smoke/app/src/main/java/org/purple/smoke/About.java
+++ Smoke/app/src/main/java/org/purple/smoke/About.java
@@ -48,7 +48,7 @@ public class About
 	if(s_about.isEmpty())
 	{
 	    s_simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-	    s_about = "Version 2019.09.09 September Sunrise " +
+	    s_about = "Version 2019.10.10 Often Optic " +
 		(BuildConfig.DEBUG ? "(Debug) " : "(Release)") +
 		"\nBuild Date " +
 		s_simpleDateFormat.format(new Date(BuildConfig.BUILD_TIME)) +
