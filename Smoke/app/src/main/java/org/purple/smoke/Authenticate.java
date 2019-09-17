c46371a2e9972d241d865d3f4b461a7e8cdf0f35
textbrowser@gmail.com <textbrowser@gmail.com>
2019-09-17 12:21:41 -0400

    Disable new menu item.

--- Smoke/app/src/main/java/org/purple/smoke/Authenticate.java
+++ Smoke/app/src/main/java/org/purple/smoke/Authenticate.java
@@ -613,6 +613,7 @@ public class Authenticate extends AppCompatActivity
 	menu.findItem(R.id.action_chat).setEnabled(isAuthenticated);
 	menu.findItem(R.id.action_fire).setEnabled(isAuthenticated);
 	menu.findItem(R.id.action_settings).setEnabled(isAuthenticated);
+	menu.findItem(R.id.action_vapor).setEnabled(isAuthenticated);
 	Miscellaneous.addMembersToMenu(menu, 4, 150);
 	return true;
     }
