83191f00bb08758ccd16ad0fd52fd42f45d44d2a
textbrowser@gmail.com <textbrowser@gmail.com>
2020-06-24 07:38:12 -0400

    New tests.

--- smoke-download-dependencies.bash
+++ smoke-download-dependencies.bash
@@ -7,8 +7,21 @@
 bouncycastle1=bcprov-ext-jdk15on-165.jar
 bouncycastle2=bcprov-jdk15on-165.tar.gz
 
+rm -f $bouncycastle1
+rm -f $bouncycastle2
 wget --progress=bar https://bouncycastle.org/download/$bouncycastle1
 wget --progress=bar https://bouncycastle.org/download/$bouncycastle2
-mv $bouncycastle1 Smoke/app/libs/.
-mv $bouncycastle2 Smoke/app/libs/.
+
+if [ -r "$bouncycastle1" ]; then
+    mv $bouncycastle1 Smoke/app/libs/.
+else
+    echo "Cannot read $bouncycastle1."
+fi
+
+if [ -r "$bouncycastle2" ]; then
+    mv $bouncycastle2 Smoke/app/libs/.
+else
+    echo "Cannot read $bouncycastle2."
+fi
+
 echo "Please review Smoke/app/build.gradle!" 
