001cf7ad779661db4be4f6cd1a4355188abdf54c
textbrowser@gmail.com <textbrowser@gmail.com>
2020-09-20 21:02:38 -0400

    Removed import.

--- Smoke/app/src/main/java/org/purple/smoke/SteamReader.java
+++ Smoke/app/src/main/java/org/purple/smoke/SteamReader.java
@@ -30,7 +30,6 @@ package org.purple.smoke;
 import android.content.res.AssetFileDescriptor;
 import android.net.Uri;
 import java.io.FileInputStream;
-import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
