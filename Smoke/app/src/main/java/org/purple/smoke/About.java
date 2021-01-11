1b866f67dfd5c76e79130d2b14c909958dfec70b
textbrowser@gmail.com <textbrowser@gmail.com>
2021-01-11 06:45:10 -0500

    New release.

--- Smoke/app/src/main/java/org/purple/smoke/About.java
+++ Smoke/app/src/main/java/org/purple/smoke/About.java
@@ -55,7 +55,7 @@ public class About
 		s_simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 		s_about = "Bouncy Castle Version " +
 		    new BouncyCastleProvider().getVersion() +
-		    "\nSmoke Version 2021.01.10 Solid Smoke (Final) " +
+		    "\nSmoke Version 2021.01.15 Solid Smoke (Final) " +
 		    (BuildConfig.DEBUG ? "(Debug) " : "(Release)") +
 		    "\nBuild Date " +
 		    s_simpleDateFormat.
@@ -68,7 +68,7 @@ public class About
 	catch(Exception exception)
 	{
 	    if(s_about.isEmpty())
-		s_about = "Smoke Version 2021.01.10 Solid Smoke (Final)";
+		s_about = "Smoke Version 2021.01.15 Solid Smoke (Final)";
 	}
 
 	return s_about;
