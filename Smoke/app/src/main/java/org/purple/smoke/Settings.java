ea71fb68d837e950e9a0f268912ca1479bd22532
textbrowser@gmail.com <textbrowser@gmail.com>
2021-07-19 10:17:50 -0400

    Silence.

--- Smoke/app/src/main/java/org/purple/smoke/Settings.java
+++ Smoke/app/src/main/java/org/purple/smoke/Settings.java
@@ -2140,6 +2140,42 @@ public class Settings extends AppCompatActivity
 	    }
         });
 
+	button1 = (Button) findViewById(R.id.silent_help);
+	button1.setOnClickListener(new View.OnClickListener()
+	{
+	    public void onClick(View view)
+	    {
+		if(Settings.this.isFinishing())
+		    return;
+
+		PopupWindow popupWindow = new PopupWindow(Settings.this);
+		TextView textView1 = new TextView(Settings.this);
+		float density = getApplicationContext().getResources().
+		    getDisplayMetrics().density;
+
+		textView1.setBackgroundColor(Color.rgb(232, 234, 246));
+		textView1.setPaddingRelative
+		    ((int) (10 * density),
+		     (int) (10 * density),
+		     (int) (10 * density),
+		     (int) (10 * density));
+		textView1.setText
+		    ("Disable all status broadcasts. Please note that " +
+		     "remote servers may terminate silent connections.");
+		textView1.setTextSize(16);
+		popupWindow.setContentView(textView1);
+		popupWindow.setOutsideTouchable(true);
+
+		if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+		{
+		    popupWindow.setHeight(450);
+		    popupWindow.setWidth(700);
+		}
+
+		popupWindow.showAsDropDown(view);
+	    }
+	});
+
 	button1 = (Button) findViewById(R.id.siphash_help);
 	button1.setOnClickListener(new View.OnClickListener()
 	{
@@ -3235,6 +3271,9 @@ public class Settings extends AppCompatActivity
 	button1.setCompoundDrawablesWithIntrinsicBounds
 	    (R.drawable.share, 0, 0, 0);
 	button1.setEnabled(isAuthenticated);
+	button1 = (Button) findViewById(R.id.silent_help);
+	button1.setCompoundDrawablesWithIntrinsicBounds
+	    (R.drawable.help, 0, 0, 0);
 	button1 = (Button) findViewById(R.id.siphash_help);
 	button1.setCompoundDrawablesWithIntrinsicBounds
 	    (R.drawable.help, 0, 0, 0);
