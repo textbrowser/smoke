9c99c92bd39841bc3e984ee4fabd9b178f85dcb7
textbrowser <textbrowser@gmail.com>
2022-09-13 07:55:32 -0400

    Find the interpreter.

--- Scripts/split.bash
+++ Scripts/split.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Alexis Megas, 2020.
 
 # Expected input file name: output.
--- adb.bash
+++ adb.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 adb="${HOME}/Android/Sdk/platform-tools/adb"
 
